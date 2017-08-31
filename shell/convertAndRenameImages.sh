e6854970d4d5d1cf2094dbae3385a0ebf748530f
simone <simonegrazioso@gmail.com>
2017-08-31 14:10:57 +0200

    shell script edited

--- shell/convertAndRenameImages.sh
+++ shell/convertAndRenameImages.sh
@@ -9,8 +9,6 @@ fi
 OBJ=$1
 a=1
 ext_1=$(printf ".%s" "$2")
-ext_2=$(printf ".%s" "$3")
-
 
 cd $OBJ
 for i in *$ext_1 ; do 
@@ -18,15 +16,6 @@ for i in *$ext_1 ; do
 	new_1=$(printf "%s_%05d.%s" "$OBJ" "$a" "$2") #04 pad to length of 5
 	convert "$i" "${new_1%.*}.$3"
   	let a=a+1 ;
-done
-
-a=1
-for i in *$ext_2 ; do 
-
-	new_2=$(printf "%s_%05d.%s" "$OBJ" "$a" "$3") #04 pad to length of 5
-	mv -i -- "$i" "$new_2"
-	echo $new_2
-	let a=a+1 ;
 done
 
 rm *$ext_1
