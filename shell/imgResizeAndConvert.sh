1465790cc79fb3315f18ed3c89d9db8be9e967a9
simone <simonegrazioso@gmail.com>
2017-09-22 10:52:26 +0200

    new script

---
+++ shell/imgResizeAndConvert.sh
@@ -0,0 +1,24 @@
+# preparing CNN dataset 
+
+
+if [ "$#" -ne 3 ]; then
+  echo echo "Usage: [obj] [image_ext_from] [image_ext_to]"
+  exit 1
+fi
+
+OBJ=$1
+a=1
+ext_1=$(printf ".%s" "$2")
+
+
+cd $OBJ
+for i in *$ext_1 ; do 
+	echo $i
+	new_1=$(printf "%s_%05d.%s" "$OBJ" "$a" "$2") #04 pad to length of 5
+	convert "$i" "${new_1%.*}.$3"
+  	let a=a+1 ;
+done
+
+rm *$ext_1
+
+
